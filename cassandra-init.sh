90a542bd11bc90ea7dfca3d0a8037ab1e0e4a615
Rjected <kidscline01@gmail.com>
2018-11-04 02:32:58 -0500

    not a god, trying literally everything just to get it all working 100%

---
+++ cassandra-init.sh
@@ -0,0 +1,10 @@
+CQL="CREATE KEYSPACE IF NOT EXISTS test_space WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+CREATE TABLE IF NOT EXISTS test_space.comments (comment varchar, name varchar, datetime timestamp, PRIMARY KEY ((name), datetime)) WITH CLUSTERING ORDER BY (datetime DESC);"
+
+
+until echo $CQL | cqlsh; do
+    echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
+    sleep 2
+done &
+
+exec /docker-entrypoint.sh "$@"
