ca5d11485d63f92d93a9e43b3e8ba6e8f6b1ea1a
Rjected <kidscline01@gmail.com>
2018-11-04 01:19:00 -0500

    fancy piping to get cqlsh script working

--- deployScripts/ApplicationStart.sh
+++ deployScripts/ApplicationStart.sh
@@ -1,2 +1,2 @@
-docker exec -it cdaemon cqlsh "CREATE KEYSPACE IF NOT EXISTS test_space WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};"
-docker exec -it cdaemon cqlsh "  CREATE TABLE IF NOT EXISTS test_space.comments (comment varchar, name varchar, datetime timestamp, PRIMARY KEY ((name), datetime)) WITH CLUSTERING ORDER BY (datetime DESC);"
+echo "CREATE KEYSPACE IF NOT EXISTS test_space WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};" | docker exec -i cdaemon cqlsh
+echo "CREATE TABLE IF NOT EXISTS test_space.comments (comment varchar, name varchar, datetime timestamp, PRIMARY KEY ((name), datetime)) WITH CLUSTERING ORDER BY (datetime DESC);" | docker exec -i cdaemon cqlsh
