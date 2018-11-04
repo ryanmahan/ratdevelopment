765b72e7d473ffaa3fd142337089b0a1749a2f79
Rjected <kidscline01@gmail.com>
2018-11-04 01:24:29 -0500

    changed to non-interactive, makes more sense

--- deployScripts/ApplicationStart.sh
+++ deployScripts/ApplicationStart.sh
@@ -1,2 +1,2 @@
-echo "CREATE KEYSPACE IF NOT EXISTS test_space WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};" | docker exec -i cdaemon cqlsh
-echo "CREATE TABLE IF NOT EXISTS test_space.comments (comment varchar, name varchar, datetime timestamp, PRIMARY KEY ((name), datetime)) WITH CLUSTERING ORDER BY (datetime DESC);" | docker exec -i cdaemon cqlsh
+echo "CREATE KEYSPACE IF NOT EXISTS test_space WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};" | docker exec cdaemon cqlsh
+echo "CREATE TABLE IF NOT EXISTS test_space.comments (comment varchar, name varchar, datetime timestamp, PRIMARY KEY ((name), datetime)) WITH CLUSTERING ORDER BY (datetime DESC);" | docker exec cdaemon cqlsh
