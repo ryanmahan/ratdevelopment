e848e25e172b5cf55030b9ac282cfcc16347e17c
gaben98 <gnathan@umass.edu>
2018-11-09 14:57:23 -0500

    query methods written, testing pending

--- DB/db.go
+++ DB/db.go
@@ -4,10 +4,12 @@ import (
 	"github.com/gocql/gocql"
 )
 
+//DatabaseSession is a container for existing cassandra session
 type DatabaseSession struct {
 	*gocql.Session
 }
 
+//NewDBSession creates a new database session.  In the future replace localhost with AWS cassandra instance
 func NewDBSession() (*DatabaseSession, error) {
 	db := gocql.NewCluster("localhost")
 	db.Keyspace = "defaultks"
--- DB/queries.go
+++ DB/queries.go
@@ -1,25 +1,32 @@
 package DB
 
+//FileBrowserDBSession is an interface for querying the database session
 type FileBrowserDBSession interface {
 	GetLatestSnapshotsByTenant(string) ([]string, error)
+	GetTimedSystemSnapshotByTenant(string, string, int) (string, error)
+	GetValidTimestampsOfSystem(string, int, int, int) ([]string, error)
 }
 
+//GetLatestSnapshotsByTenant returns slice of JSON blobs for the latest snapshots of all systems owned by a tenant
 func (db *DatabaseSession) GetLatestSnapshotsByTenant(tenant string) ([]string, error) {
 	return db.RunQuery("SELECT snapshot FROM latest_snapshots_by_tenant WHERE tenant = ?", tenant)
 }
 
-func (db *DatabaseSession) GetTimedSystemSnapshotByTenant(tenant string, time string, sernum int) (string, error) {
-	snapshots, err = db.RunQuery("SELECT snapshot FROM snapshots_by_serial_number WHERE tenant = ? AND serial_number = ? AND time = ?", tenant, sernum, time)
+//GetTimedSystemSnapshotByTenant gets the JSON blob of a system at a specified timestamp
+func (db *DatabaseSession) GetTimedSystemSnapshotByTenant(tenant, time string, sernum int) (string, error) {
+	snapshots, err := db.RunQuery("SELECT snapshot FROM snapshots_by_serial_number WHERE tenant = ? AND serial_number = ? AND time = ?", tenant, sernum, time)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	return snapshots[0], nil
 }
 
+//GetValidTimestampsOfSystem returns a slice of strings that represent valid timestamps to index by for a system
 func (db *DatabaseSession) GetValidTimestampsOfSystem(tenant string, sernum, offset, count int) ([]string, error) {
 	return db.RunQuery("SELECT time FROM snapshots_by_serial_number WHERE tenant = ? AND serial_number = ?", tenant, sernum)
 }
 
+//RunQuery is a helper function to easily get slice of strings that is returned from query, with error handling
 func (db *DatabaseSession) RunQuery(query string, args ...interface{}) ([]string, error) {
 	iter := db.Session.Query(query, args...).Iter()
 
