332c80042b912f29f98082dcdaceb13860c80be4
Artur Tkachenko <altkachenko@umass.edu>
2018-11-12 16:56:59 -0500

    added comments to things

--- DB/db.go
+++ DB/db.go
@@ -2,6 +2,7 @@ package DB
 
 import (
 	"github.com/gocql/gocql"
+	"time"
 )
 
 type DatabaseSession struct {
@@ -10,6 +11,9 @@ type DatabaseSession struct {
 
 func NewDBSession() (*DatabaseSession, error) {
 	db := gocql.NewCluster("localhost")
+	// Datadumps are big and gocql times out by default while loading it in
+	// so we extend the time out duration
+	db.ConnectTimeout = time.Minute * 10
 	db.Keyspace = "defaultks"
 	session, err := db.CreateSession()
 	if err != nil {
