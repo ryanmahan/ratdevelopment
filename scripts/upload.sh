749ceb8a5c8a65e86bb4edc8dd4c5ef40a2c79df
amarzot <43010352+amarzot@users.noreply.github.com>
2018-11-12 22:08:11 -0500

    remove old script

--- scripts/upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Takes in the directory containing the datadumps
-# Runs the mouse_upload program to load cassandra with the datadumps
-# Resolve the directory of this script
-SCRIPT_DIR=$(dirname $(readlink -f "$0"))
-go run "$SCRIPT_DIR/mouse_upload/main.go" $1
