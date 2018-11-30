e3b926c2ba92d4f0c0c875a2300db996057c754a
Rjected <kidscline01@gmail.com>
2018-11-30 14:51:00 -0500

    made mistake with verifycassandra

--- deployScripts/VerifyCassandra.sh
+++ deployScripts/VerifyCassandra.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-while ! cqlsh -e 'describe cluster' ; do
-  sleep 1
-done
 killall go
 rm -rf /go > /goresult.txt
 sudo service cassandra restart
+while ! cqlsh -e 'describe cluster' ; do
+    sleep 1
+done
