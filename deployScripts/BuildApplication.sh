273d819b5feb1072fa200c4e9dc320d8fa4584d4
Rjected <kidscline01@gmail.com>
2018-11-30 21:51:57 -0500

    not everyone make aliases like you dan

--- deployScripts/BuildApplication.sh
+++ deployScripts/BuildApplication.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export GOPATH=/go
 cd /go/src/ratdevelopment-backend
-sudo rm ~/gobuildlogs.txt
-touch ~/gobuildlogs.txt
-go get 2>>~/gobuildlogs.txt
-go build 2>>~/gobuildlogs.txt
-go install 2>>~/gobuildlogs.txt
+sudo rm /home/ubuntu/gobuildlogs.txt
+touch /home/ubuntu/gobuildlogs.txt
+go get 2>>/home/ubuntu/gobuildlogs.txt
+go build 2>>/home/ubuntu/gobuildlogs.txt
+go install 2>>/home/ubuntu/gobuildlogs.txt
