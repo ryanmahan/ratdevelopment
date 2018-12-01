87c5d1b8a0afbc59cb560588f7c1d07ae61c23ab
Rjected <kidscline01@gmail.com>
2018-11-30 21:49:15 -0500

    stop making things in the root directory or it wont work dan

--- deployScripts/BuildApplication.sh
+++ deployScripts/BuildApplication.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 export GOPATH=/go
 cd /go/src/ratdevelopment-backend
-go get
-go build
-go install
+sudo rm ~/gobuildlogs.txt
+touch ~/gobuildlogs.txt
+go get 2>>~/gobuildlogs.txt
+go build 2>>~/gobuildlogs.txt
+go install 2>>~/gobuildlogs.txt
