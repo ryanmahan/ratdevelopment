97ba25cebc66adf6654edcf3a56f9a1dc3365ef6
Rjected <kidscline01@gmail.com>
2018-12-04 13:45:50 -0500

    changed append for logs

--- deployScripts/BuildApplication.sh
+++ deployScripts/BuildApplication.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 export GOPATH=/go
 cd /go/src/ratdevelopment-backend
-echo "=== GET STEP START ===" > /home/ubuntu/gobuildlogs.txt 2>&1
-go get -v >>/home/ubuntu/gobuildlogs.txt 2>&1
-echo "=== GET STEP DONE ===" >> /home/ubuntu/gobuildlogs.txt 2>&1
-echo "=== BUILD STEP START ===" >> /home/ubuntu/gobuildlogs.txt 2>&1
-go build >>/home/ubuntu/gobuildlogs.txt 2>&1
-echo "=== BUILD STEP DONE ===" >> /home/ubuntu/gobuildlogs.txt 2>&1
-echo "=== INSTALL STEP START ===" >> /home/ubuntu/gobuildlogs.txt 2>&1
-go install >>/home/ubuntu/gobuildlogs.txt 2>&1
-echo "=== INSTALL STEP DONE ===" >> /home/ubuntu/gobuildlogs.txt 2>&1
+echo "=== GET STEP START ===" &> /home/ubuntu/gobuildlogs.txt
+go get -v &>> /home/ubuntu/gobuildlogs.txt
+echo "=== GET STEP DONE ===" &>> /home/ubuntu/gobuildlogs.txt
+echo "=== BUILD STEP START ===" &>> /home/ubuntu/gobuildlogs.txt
+go build &>> /home/ubuntu/gobuildlogs.txt
+echo "=== BUILD STEP DONE ===" &>> /home/ubuntu/gobuildlogs.txt
+echo "=== INSTALL STEP START ===" &>> /home/ubuntu/gobuildlogs.txt
+go install &>> /home/ubuntu/gobuildlogs.txt
+echo "=== INSTALL STEP DONE ===" &>> /home/ubuntu/gobuildlogs.txt
