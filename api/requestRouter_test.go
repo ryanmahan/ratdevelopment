5be3fee4187d28ad7b97a33ff423bcbc0be51c05
Rjected <kidscline01@gmail.com>
2018-12-06 16:46:49 -0500

    created test files

---
+++ api/requestRouter_test.go
@@ -0,0 +1,10 @@
+package api
+
+import (
+	"testing"
+)
+
+func routerTest(t *testing.T) {
+	// TODO just test basic router functions to ensure wrapper works
+	return
+}
---
+++ api/server_test.go
@@ -0,0 +1,10 @@
+package api
+
+import (
+	"testing"
+)
+
+func testServer(t *testing.T) {
+	// TODO: test everything, bring tests over from endpoints
+	return
+}
