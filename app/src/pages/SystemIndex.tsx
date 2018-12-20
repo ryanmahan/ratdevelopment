5a737c64470a530803555dcf6ab6c0a62b818d51
gaben98 <gnathan@umass.edu>
2018-12-19 23:51:10 -0500

    some changes towards hooking up frontend

--- app/src/pages/SystemIndex.tsx
+++ app/src/pages/SystemIndex.tsx
@@ -1,29 +1,33 @@
 import * as React from "react";
 import '../sass/custom-bulma.scss';
-import {Divider} from "../components/layout/Divider";
-import {PageTitle} from "../components/layout/PageTitle";
-import {SystemIndexTable} from "../components/SystemIndexComponents/SystemIndexTable";
-import {SearchBar} from "../components/SystemIndexComponents/SearchBar"
+import { Divider } from "../components/layout/Divider";
+import { PageTitle } from "../components/layout/PageTitle";
+import { SystemIndexTable } from "../components/SystemIndexComponents/SystemIndexTable";
+import { SearchBar } from "../components/SystemIndexComponents/SearchBar"
 
 //import * as sample from "./SystemViewComponents/SampleSystem.json";
 
 export interface SystemIndexProps {
 
 }
- 
+
 export interface SystemIndexState {
-    snapshotArray: any[]
+	snapshotArray: any[]
+	searchstring: string
 }
 export class SystemIndex extends React.Component<SystemIndexProps, SystemIndexState> {
+	handleSearchChange(query: string) {
+		this.setState({ searchstring: query });
+	}
 
-    render() {
-        return (
-            <div className="container">
-                <SearchBar/>
-                <PageTitle title={"Systems"}/>
-                <Divider/>
-                <SystemIndexTable/>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="container">
+				<SearchBar />
+				<PageTitle title={"Systems"} />
+				<Divider />
+				<SystemIndexTable searchstring={this.state.searchstring} />
+			</div>
+		)
+	}
 }
